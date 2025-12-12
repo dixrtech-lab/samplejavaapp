ca3bbda4daa6f3df1237a7657af6f228ba50fb74
dixrtech-lab <dixrtech@gmail.com>
2025-12-12 13:50:51 +0100

    Update ContactForm.java

--- src/main/java/com/devopsdemo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/devopsdemo/tutorial/addressbook/ContactForm.java
@@ -9,7 +9,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 
-/* Create custom UI Componentssssssss.
+/* Create custom UI Componentsssssssss.
  *
  * Create your own Vaadin components by inheritance and composition.
  * This is a form component inherited from VerticalLayout. Use
